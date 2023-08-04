336f3f41c7511e47045c9ecd713cbc4a8364c1c1
asmaa <asmaaabdelfattah491@yahoo.com>
2023-08-04 20:04:43 +0300

    Recipe Details Screen

--- reciptApp/Model/recipe.swift
+++ reciptApp/Model/recipe.swift
@@ -12,23 +12,11 @@ struct Recipe: Decodable{
     var name: String
     var time: String
     var image: String
-    //var weeks: [String]
     var carbos: String
     var fibers: String
-    var rating: Double?
     var country: String
-   // var ratings: Int?
     var calories: String
-    var headline: String
-    var keywords: [String]
-    var products: [String]
     var proteins: String
-    var favorites: Int
-   // var difficulty: Int
     var description: String
-    var highlighted: Bool
     var ingredients: [String]
-  //  var incompatibilities: String?
-    var deliverable_ingredients: [String]
-    var undeliverable_ingredients: [String]
 }
